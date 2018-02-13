3af15bae9a5cb03aca736b849bd43e0842518ae5
m.hoffmann@roots48.com <marc hoffmann@sander.lan>
2018-02-13 12:09:18 +0100

    Added Player Connection Handling and Player Position Handling

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
-io.sockets.on('connection', function(socket) {
-    console.log("Client has connected!");
-  
-  socket.on('playerJoined', function(msg){
-    console.log("Message -> ",msg);
+
+
+var curPlayer = [];
+
+
+function Player(id) {
+  this.id = id;
+  this.pos = {
+    x: 0,
+    y: 0,
+    z: 0
+  };
+  this.entity = null;
+}
+
+
+
+io.sockets.on('connection', function (socket) {
+  socket.on('initialize', function(){
+    var id = socket.id;
+    var newPlayer = new Player(id);
+
+
+    curPlayer[id]=newPlayer;
+
+    socket.emit('playerData', {id: id, 'currentPlayer': curPlayer});
+
+    socket.broadcast.emit('playerJoined', newPlayer);
   });
-  
-  
-  
-  
+
+  socket.on('positionUpdate', function(data){
+    curPlayer[data.id].x = data.x;
+    curPlayer[data.id].y = data.y;
+    curPlayer[data.id].z = data.z;
+
+    socket.broadcast.emit('playerMoved', data);
+  });
+
+
+
+
 });
 
-console.log ('Server started.');
+console.log('Server started.');
 server.listen(3000);
