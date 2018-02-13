b94b566f810079c7e735c4260242f4e1d43d360a
Glitch (mature-literature) <none>
2018-02-13 13:03:31 +0000

    🛫🐽 Updated with Glitch

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
-io.sockets.on('connection', function(socket) {
-    console.log("Client has connected!");
-  
-  socket.on('playerJoined', function(msg){
-    console.log("Message -> ",msg);
+
+
+var curPlayer = [];
+
+
+function Player(id) {
+  this.id = id;
+  this.x = 0;
+  this.y = 0;
+  this.z = 0;
+  this.entity = null;
+}
+
+
+
+io.sockets.on('connection', function (socket) {
+  socket.on('initialize', function(){
+    console.log("New User connected and initialized!");
+    var id = socket.id;
+    var newPlayer = new Player(id);
+
+
+    curPlayer[id]=newPlayer;
+
+    socket.emit('playerData', {id: id, 'currentPlayer': curPlayer});
+
+    socket.broadcast.emit('playerJoined', newPlayer);
   });
-  
-  
-  
-  
+
+  socket.on('positionUpdate', function(data){
+    curPlayer[data.id].x = data.x;
+    curPlayer[data.id].y = data.y;
+    curPlayer[data.id].z = data.z;
+
+    socket.broadcast.emit('playerMoved', data);
+  });
+
+  socket.on('disconnect', function(){
+    socket.broadcast.emit('playerLeft',this.id);
+  });
+
+
+
+
 });
 
-console.log ('Server started.');
+console.log('Server started.');
 server.listen(3000);
